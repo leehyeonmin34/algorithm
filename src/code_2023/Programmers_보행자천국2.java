5b766deda03ff855db2c7afb2987ef46338d4ea9
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-11-17 12:30:38 +0900

    보행자천국 풀이 완료

--- src/code_2023/Programmers_보행자천국2.java
+++ src/code_2023/Programmers_보행자천국2.java
@@ -17,59 +17,36 @@ public class Programmers_보행자천국2 {
     final int RIGHT = 1;
     int M = 0;
     int N = 0;
-    enum Sign{
-            VALID(0),
-        INVALID(1),
-        UNTURNABLE(2)
-                    ;
-        public final int value;
-        private Sign(int value){
-            this.value = value;
-        }
-    }
+    final int VALID = 0;
+    final int UNTURNABLE = 2;
 
     int solution(int m, int n, int[][] cityMap) {
         int[][][] route = new int[m + 1][n + 1][2];
         M = m;
         N = n;
 
+
         // 제일먼저 위에서 아래로, 점점 오른쪽으로 경로수를 찾아나감
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 switch (cityMap[i][j]){
-                    case 0:
+                    case VALID:
                         int routeNumToNext = i == 0 && j == 0 ? 1 : routeNumTo(i, j, route);
-                        route[i + 1][j][DOWN] = routeNumToNext;
-                        route[i][j + 1][RIGHT] = routeNumToNext;
-                        makeZeroIfNextPointIsInvalid(i, j, route, cityMap);
+                        route[i + 1][j][DOWN] = routeNumToNext % MOD;
+                        route[i][j + 1][RIGHT] = routeNumToNext % MOD;
                         continue;
-                    case 2:
-                        route[i + 1][j][DOWN] = route[i][j][DOWN];
-                        route[i][j + 1][RIGHT] = route[i][j][RIGHT];
-                        makeZeroIfNextPointIsInvalid(i, j, route, cityMap);
+                    case UNTURNABLE:
+                        route[i + 1][j][DOWN] = route[i][j][DOWN] % MOD;
+                        route[i][j + 1][RIGHT] = route[i][j][RIGHT] % MOD;
                         continue;
                 }
             }
         }
-        System.out.println(Arrays.deepToString(route));
         return routeNumTo(m - 1, n - 1, route) % MOD;
     }
 
-
     int routeNumTo(int i, int j, int[][][] route){
         return route[i][j][RIGHT] + route[i][j][DOWN];
-    }
-
-    void makeZeroIfNextPointIsInvalid(int i, int j, int route[][][], int[][] map){
-        makeZeroIfOne(i + 1, j, route, map);
-        makeZeroIfOne(i, j + 1, route, map);
-    }
-
-    void makeZeroIfOne(int i, int j, int route[][][], int[][] map){
-        if (i < M && j < N && map[i][j] == Sign.INVALID.value){
-            route[i][j][RIGHT] = 0;
-            route[i][j][DOWN] = 0;
-        }
     }
 
 }

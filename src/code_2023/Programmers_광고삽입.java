7a25210ada05ea91a3ab01ac09c94a3f71c26e02
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-11-18 17:42:29 +0900

    Stream, List를 배열로 변환

--- src/code_2023/Programmers_광고삽입.java
+++ src/code_2023/Programmers_광고삽입.java
@@ -1,20 +1,16 @@
 package code_2023;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 public class Programmers_광고삽입 {
     public static void main(String[] args) {
         Programmers_광고삽입 solution = new Programmers_광고삽입();
 
         // "01:30:59"
-//        System.out.println(solution.solution("02:03:55"	, "00:14:15", new String[] {"01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"}));
+        System.out.println(solution.solution("02:03:55"	, "00:14:15", new String[] {"01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"}));
 
         // "01:00:00"
-//        System.out.println(solution.solution("99:59:59",	"25:00:00", new String[] {"69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"}));
+        System.out.println(solution.solution("99:59:59",	"25:00:00", new String[] {"69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"}));
 
         // "00:00:00"
         System.out.println(solution.solution("50:00:00", "50:00:00", new String[] {"15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"}));
@@ -33,6 +29,12 @@ public class Programmers_광고삽입 {
         // 광고구간이 재생구간을 지나는 슬라이딩윈도우라고 생각했을 때,
         // '윈도우의 앞과 뒤의 시청인원이 변하게되는 윈도우 위치'의 앞 지점(광고 시작시간)들을 리스트로 만든다.
         // 각 지점으로 이동할 때 마다, 누적 뷰 수의 변화는 (현재시간 - 이전지점 시간) * (윈도우 끝 인원 - 윈도우 시작 인원)임을 이용해 view 수를 갱신하는 것
+        //
+        // 단점
+        // 이 알고리즘은 초단위로 누적 view 수를 계산하지 않고, 한 개의 log마다 4번의 계산을 거친다.
+        // 최대 playTime은 99*59*59 = 344,619이므로 344619/4 = 86,154.75 개 이상의 log가 있을 때 초단위 계산보다 계산을 많이하게 된다.
+        // 영상이 100시간이 넘어갈 일보다 view log가 8.6만개보다 많을 일이 훨씬 많기 때문에 테스트케이스에서 초단위 갱신보다 더 느리다.
+        // 초단위보다 뭔가 더 빠르게 할 수 있지 않을까 싶어서 이렇게 해봤지만 오히려 더 느릴 것 같다.
 
         final int START = 0; // 재생 시작 (광고 시작할 때 인원 +1)
         final int START_AFTER_AD = 1; // 광고 구간 끝에 재생 시작 (광고 끝나면 인원 +1)
@@ -41,23 +43,22 @@ public class Programmers_광고삽입 {
 
         final int play = getSecondFromTimeString(playTime);
         final int adv = getSecondFromTimeString(advTime);
-        prevSec = -adv;
+        prevSec = -adv; // 슬라이딩윈도우의 오른쪽끝이 0초에 닿아있을때부터 시작함.
 
         // 윈도우의 앞과 뒤의 시청인원이 변하게되는 윈도우 위치의 앞 지점
-        List<int[]> times = Arrays.stream(logs).map(log -> {
-            String[] startAndEndTime = log.split("-");
+        int[][] times = new int[logs.length * 4][2];
+        for(int i = 0; i < logs.length; i++) {
+            String[] startAndEndTime = logs[i].split("-");
             int start = getSecondFromTimeString(startAndEndTime[0]);
             int end = getSecondFromTimeString(startAndEndTime[1]);
-            return new int[][]{
-                    {start, START}
-                    , {start - adv, START_AFTER_AD}
-                    , {end, END}
-                    , {end - adv, END_AFTER_AD}};
-            }).flatMap(Arrays::stream)
-            .sorted(Comparator.comparingInt(a -> a[0])) // 정렬 해줘야 윈도우 슬라이딩이 가능
-            .collect(Collectors.toList());
-//        System.out.println(Arrays.deepToString(times.toArray()));
+            times[i * 4] = new int[] {start, START};
+            times[i * 4 + 1] = new int[] {start - adv, START_AFTER_AD};
+            times[i * 4 + 2] = new int[] {end, END};
+            times[i * 4 + 3] = new int[] {end - adv, END_AFTER_AD};
+        }
+        Arrays.sort(times, (a, b) -> a[0] - b[0]);
 
+        // 각 지점들을 돌며 view 수 갱신
         for(int[] time : times){
             currSec = time[0];
             int pointType = time[1];
@@ -118,19 +119,17 @@ public class Programmers_광고삽입 {
 
     private int getSecondFromTimeString(String timeString){
         String[] timeFrags = timeString.split(":");
-        int hour = Integer.valueOf(timeFrags[0]);
-        int min = Integer.valueOf(timeFrags[1]);
-        int sec = Integer.valueOf(timeFrags[2]);
-        return hour * 60 * 60 + min * 60 + sec;
+        int hour = Integer.parseInt(timeFrags[0]);
+        int min = Integer.parseInt(timeFrags[1]);
+        int sec = Integer.parseInt(timeFrags[2]);
+        return hour * 3600 + min * 60 + sec;
     }
 
     private String getTimeStringFromSec(int secInt){
-        int hour = secInt / (60 * 60);
+        int hour = secInt / (3600);
         int min = (secInt / 60) % 60;
         int sec = secInt % 60;
-        return Arrays.stream(new int[]{hour, min, sec})
-                .mapToObj(value -> String.format("%02d", value))
-                .collect(Collectors.joining(":"));
+        return String.format("%02d:%02d:%02d", hour, min, sec);
     }
 
 
